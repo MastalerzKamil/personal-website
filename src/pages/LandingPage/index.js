3970eced891bd47da6d5ec4980c4625ba5f22976
Kamil Mastalerz <kamil.mastalerz7@wp.pl>
2020-02-23 18:43:01 +0100

    Add hyperlinks to socialmedia

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import pcImage from 'img/landing-page.jpg'
-import meImage from 'img/me-photo.jpg'
+import pcImage from 'img/landing-page.jpg';
+import meImage from 'img/me-photo.jpg';
+import githubLogo from 'img/logo/GitHub.png';
+import linkedInLogo from 'img/logo/LinkedIn.png';
 
 import './index.css';
 
@@ -14,6 +16,14 @@ function LandingPage() {
         <img src={meImage} className='LandingPage__mePhoto' alt='me' />
         <div className='LandingPage__name'>Kamil Mastalerz</div>
         <div className='LandingPage__description'>Full-Stack developer available for hire</div>
+        <div className='LandingPage__links'>
+          <a href='https://github.com/MastalerzKamil'>
+            <img src={githubLogo} className='LandingPage__link' alt='github' />
+          </a>
+          <a href='https://www.linkedin.com/in/kamil-mastalerz'>
+            <img src={linkedInLogo} className='LandingPage__link' alt='github' />
+          </a>
+        </div>
       </div>
     </div>
   )
