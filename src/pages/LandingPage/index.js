71e561f2df260ff710357e571b20fcceee74ae39
Kamil Mastalerz <kamil.mastalerz7@wp.pl>
2020-02-23 17:46:09 +0100

    Add info about author

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import pcImage from 'img/landing-page.jpg'
+import meImage from 'img/me-photo.jpg'
 
 import './index.css';
 
@@ -7,9 +8,13 @@ function LandingPage() {
   return (
     <div className='LandingPage'>
       <div className='split LandingPage__left'>
-        <img src={pcImage} alt='landing-page'/>
+        <img src={pcImage} className='LandingPage__leftBackground' alt='landing-page'/>
       </div>
-      <div className='split LandingPage__right'></div>
+      <div className='split LandingPage__right'>
+        <img src={meImage} className='LandingPage__mePhoto' alt='me' />
+        <div className='LandingPage__name'>Kamil Mastalerz</div>
+        <div className='LandingPage__description'>Full-Stack developer available for hire</div>
+      </div>
     </div>
   )
 }
