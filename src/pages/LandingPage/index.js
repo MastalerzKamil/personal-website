79eb9f7508a130869b56247b11b917e050a619e2
Kamil Mastalerz <kamil.mastalerz7@wp.pl>
2020-02-29 15:58:41 +0100

    Add wave

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -31,6 +31,9 @@ function LandingPage() {
             <img src={linkedInLogo} className='LandingPage__link' alt='linkedin' />
           </a>
       </div>
+      <div className='LandingPage__move'>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#2B3740" fill-opacity="1" d="M0,256L1440,320L1440,320L0,320Z"></path></svg>
+      </div>
       <div className='LandingPage__background'></div>
     </div>
   );
--- src/pages/Skills/index.js
+++ src/pages/Skills/index.js
@@ -3,6 +3,8 @@ import reactLogo from 'img/logo/react.png';
 import dockerLogo from 'img/logo/docker.png';
 import nodeLogo from 'img/logo/node.png';
 
+import './index.css';
+
 function Skills() {
   return (
     <div className='Skills'>
@@ -10,9 +12,9 @@ function Skills() {
         Main technologies
       </div>
       <div className='Skills__technologies'>
-        <img src={reactLogo} alt='react' />
-        <img src={nodeLogo} alt='node' />
-        <img src={dockerLogo} alt='node' />
+        <img className='Skills__img' src={reactLogo} alt='react' />
+        <img className='Skills__img' src={nodeLogo} alt='node' />
+        <img className='Skills__img' src={dockerLogo} alt='node' />
       </div>
     </div>
   );
